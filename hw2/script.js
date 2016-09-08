0f94423519f7040b6adb59a73d702621ebecf665
Brandon Tobin <brandontobin@cox.net>
2016-09-08 17:42:06 -0600

    Everything working besides the scatter plot when random is checked. Still need to fix this.

--- hw2/script.js
+++ hw2/script.js
@@ -2,9 +2,8 @@
 // These keep JSHint quiet if you're using it (highly recommended!)
 
 function staircase() {
-    // ****** TODO: PART II - Need to fix random underbar line******
 
-    var bars = document.getElementById("bars").children;
+    var bars = document.getElementById("bars-a").children;
     var widthValues = [];
 
     for (i of bars)
@@ -63,48 +62,66 @@ function update(error, data) {
 
     // ****** TODO: PART III (you will also edit in PART V) ******
 
-    // TODO: Select and update the 'a' bar chart bars
-    var svg = d3.selectAll("svg");
+    // Select and update the 'a' bar chart bars
+    var svg = d3.selectAll("#bars-a");
 
-    var bars = svg.selectAll(".bars-a").data(data);
+    var bars = svg.selectAll("rect").data(data);
 
-    bars.merge(bars);
+    bars.exit()
+        .transition()
+        .duration(1000)
+        .attr("opacity", 0)
+        .remove();
 
-    // bars.attr("width", function (d) {
-    //         return d.a * 10;
-    //     })
-    bars.attr("width", function (d){
-        return aScale(d);
-    })
-        .attr("height", 20)
+    bars = bars.enter()
+        .append("rect")
+        .merge(bars)
+        .transition()
+        .duration(3000)
+        .attr("opacity", 1);
+
+    bars.attr("x", 0)
         .attr("y", function (d, i) {
-            console.log((i * 20) + 20);
-            return (i * 20) + 20;
+            return iScale(i) + 10;
         })
+        .attr("width", function (d) {
+            var test = aScale(d.a);
+            return aScale(d.a);
+        })
+        .attr("height", 10)
+        .style("fill", "steelblue");
+
+
+    // Select and update the 'b' bar chart bars
+    var svg = d3.selectAll("#bars-b");
+
+    var bars = svg.selectAll("rect").data(data);
+
+    bars.exit()
+        .transition()
+        .duration(1000)
+        .attr("opacity", 0)
+        .remove();
+
+    bars = bars.enter()
+        .append("rect")
+        .merge(bars)
+        .transition()
+        .duration(3000)
+        .attr("opacity", 1);
+
+    bars.attr("x", 0)
+        .attr("y", function (d, i) {
+            return iScale(i) + 10;
+        })
+        .attr("width", function (d) {
+            return bScale(d.b);
+        })
+        .attr("height", 10)
         .style("fill", "steelblue")
         .attr("opacity", 1);
 
-    // TODO: Select and update the 'b' bar chart bars
-    var svg = d3.selectAll("svg");
-
-    var bars = svg.selectAll(".bars-b").data(data);
-
-    bars.merge(bars);
-
-    bars.attr("width", function (d) {
-        return d.b * 10;
-    })
-        .attr("height", 20)
-        .attr("y", function (d, i) {
-            console.log((i * 20) + 20);
-            return (i * 20) + 20;
-        })
-        .style("fill", "steelblue")
-        .attr("opacity", 1)
-        .attr("stroke", "darkgray")
-        .attr("stroke-width", 1);
-
-    // TODO: Select and update the 'a' line chart path using this line generator
+    // Select and update the 'a' line chart path using this line generator
     var aLineGenerator = d3.line()
         .x(function (d, i) {
             return iScale(i);
@@ -113,9 +130,28 @@ function update(error, data) {
             return aScale(d.a);
         });
 
-    // TODO: Select and update the 'b' line chart path (create your own generator)
+    var svg = d3.selectAll("svg#line-a");
 
-    // TODO: Select and update the 'a' area chart path using this line generator
+    svg.selectAll("path")
+        .data(data)
+        .attr("d", aLineGenerator(data));
+
+    // TODO: Select and update the 'b' line chart path (create your own generator)
+    var bLineGenerator = d3.line()
+        .x(function (d, i) {
+            return iScale(i);
+        })
+        .y(function (d) {
+            return bScale(d.b);
+        });
+
+    var svg = d3.selectAll("svg#line-b");
+
+    svg.selectAll("path")
+        .data(data)
+        .attr("d", bLineGenerator(data));
+
+    // Select and update the 'a' area chart path using this line generator
     var aAreaGenerator = d3.area()
         .x(function (d, i) {
             return iScale(i);
@@ -125,11 +161,93 @@ function update(error, data) {
             return aScale(d.a);
         });
 
-    // TODO: Select and update the 'b' area chart path (create your own generator)
+    var svg = d3.selectAll("svg#area-a");
+
+    svg.selectAll("path")
+        .data(data)
+        .attr("d", aAreaGenerator(data));
+
+    // Select and update the 'b' area chart path (create your own generator)
+    var bAreaGenerator = d3.area()
+        .x(function (d, i) {
+            return iScale(i);
+        })
+        .y0(0)
+        .y1(function (d) {
+            return bScale(d.b);
+        });
+
+    var svg = d3.selectAll("svg#area-b");
+
+    svg.selectAll("path")
+        .data(data)
+        .attr("d", bAreaGenerator(data));
 
     // TODO: Select and update the scatterplot points
+    var svg = d3.selectAll("svg#scatter");
 
-    // ****** TODO: PART IV ******
+    var circles = svg.selectAll("circle").data(data);
+
+    circles.exit()
+        .transition()
+        .duration(1000)
+        .attr("opacity", 0)
+        .remove();
+
+    circles = circles.enter()
+        .append("circle")
+        .merge(circles);
+
+    circles
+        .transition()
+        .duration(3000)
+        .attr("opacity", 1);
+
+    circles.attr("cx", function (d) {
+            return aScale(d.a);
+        })
+        .attr("cy", function (d) {
+            return bScale(d.b);
+        })
+        .attr("r", 5);
+
+
+    // PART IV
+
+    // Change bar color on hover
+    d3.selectAll("rect")
+        .on("mouseover", function (d) {
+            d3.select(this)
+                .style("fill", "Red");
+        })
+        .on("mouseout", function (d) {
+            d3.select(this)
+                .style("fill", "steelblue");
+        });
+
+    // Scatter Plot Click / Hover
+    var coordView = d3.select("body").append("div")
+        .attr("class", "coordView")
+        .style("opacity", 0);
+
+    svg.selectAll("circle")
+        .on("click", function(d) {
+            var x = aScale(d.a);
+            var y = bScale(d.b);
+            console.log("You Clicked on Scatter Point: " + x + "," + y);
+        })
+        .on("mouseover", function (d) {
+            coordView.transition()
+                .style("opacity", 1);
+            coordView.html("Mouse is Over Scatter Point: <br /> (" + aScale(d.a) + ", " + bScale(d.b) + ")")
+                .style("left", (d3.event.pageX) + 10 + "px")
+                .style("top", (d3.event.pageY) + 10 + "px");
+        })
+        .on("mouseout", function () {
+            coordView.transition()
+                .duration(400)
+                .style("opacity", 0);
+        });
 }
 
 function changeData() {
